04865de19aaefd8b1f702290f58c1be11b5f841a
paulocesarvasco <paulocvasco.93@gmail.com>
2021-04-04 18:17:58 -0300

    Method to add new songs in playlist

--- 175547_Lab01/src/com/unicamp/mc322/lab02/src/com/unicamp/mc322/lab02/Playlist.java
+++ 175547_Lab01/src/com/unicamp/mc322/lab02/src/com/unicamp/mc322/lab02/Playlist.java
@@ -1,11 +1,25 @@
 package com.unicamp.mc322.lab02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Playlist {
 	private String name;
 	private String rhythm;
 	
+    private List<Song> songs = new ArrayList<Song>();
+
+	
 	public Playlist(String name, String rhythm) {
 		this.name = name;
 		this.rhythm = rhythm;
+	}
+	
+	public boolean addSong(Song song) {
+		
+		boolean error = false;
+		
+		error = this.songs.add(song);
+		return !error;
 	}
 }
