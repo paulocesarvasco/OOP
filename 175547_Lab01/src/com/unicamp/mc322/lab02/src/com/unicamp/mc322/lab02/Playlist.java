fe6d86b069e719b1be87a46faf8af69f56afe035
paulocesarvasco <paulocvasco.93@gmail.com>
2021-04-04 20:43:36 -0300

    Method to show songs

--- 175547_Lab01/src/com/unicamp/mc322/lab02/src/com/unicamp/mc322/lab02/Playlist.java
+++ 175547_Lab01/src/com/unicamp/mc322/lab02/src/com/unicamp/mc322/lab02/Playlist.java
@@ -22,4 +22,16 @@ public class Playlist {
 		error = this.songs.add(song);
 		return !error;
 	}
+	
+	public boolean showSongs(int index) {
+		
+		boolean error = false;
+		System.out.printf("Playlist %d: %s\n", index, this.name);
+		System.out.printf("		Number of Songs: %d\n", this.songs.size());
+		System.out.println("	Songs:");
+		for (Song song : this.songs) {
+			song.showInfo();
+		}
+		return error;
+	}
 }
