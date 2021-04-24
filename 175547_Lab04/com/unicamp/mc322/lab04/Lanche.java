f956f392d5f3d33e0182ffd8966bfcc5b88663a4
paulocesarvasco <paulocvasco.93@gmail.com>
2021-04-24 15:21:31 -0300

    Create class 'Lanche'

---
+++ 175547_Lab04/com/unicamp/mc322/lab04/Lanche.java
@@ -0,0 +1,15 @@
+package com.unicamp.mc322.lab04;
+
+public class Lanche {
+
+	private String identificador;
+	private String nome;
+	private double preco;
+	
+	// Construtor
+	public Lanche(String identificador, String nome, double preco) {
+		this.identificador = identificador;
+		this.nome = nome;
+		this.preco = preco;
+	}
+}
