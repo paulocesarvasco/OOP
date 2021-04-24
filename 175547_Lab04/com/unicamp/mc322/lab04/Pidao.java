c09dc02fca97ebd1f24622519d76a6f26302cc46
paulocesarvasco <paulocvasco.93@gmail.com>
2021-04-24 17:35:27 -0300

    Methods to 'Pidao'

--- 175547_Lab04/com/unicamp/mc322/lab04/Pidao.java
+++ 175547_Lab04/com/unicamp/mc322/lab04/Pidao.java
@@ -1,10 +1,18 @@
 package com.unicamp.mc322.lab04;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pidao {
 
 	private String nome;
 	private String cnpj;
 	private int endereco[] = new int[2];
+	
+	// Lista que armezena todos usuarios cadastrados no app
+	private List<User> users = new ArrayList<User>();
+	// Lista com o nome de todos os lanches adicionados
+	private List<Lanche> cardapio = new ArrayList<Lanche>();
 	
 	// Construtor
 	public Pidao(String nome, String cnpj, int X, int Y) {
@@ -12,5 +20,45 @@ public class Pidao {
 		this.cnpj = cnpj;
 		this.endereco[0] = X;
 		this.endereco[1] = Y;
+	}
+	
+	public User cadastrarUsuario(String nome, String cpf, int X, int Y) {
+		User newUser = new User(nome, cpf, X, Y);
+		this.users.add(newUser);
+		return newUser;
+	}
+	
+	public void adicionarAoCardapio(Lanche lanche) {
+		this.cardapio.add(lanche);
+	}
+	
+	public void aplicarDesconto(String identificador, double porcentagem, TipoDesconto desconto) {
+		
+		Lanche item = buscarItem(identificador);
+		if (item == null) {
+			return;
+		}
+		switch (desconto) {
+		case PORCENTAGEM:
+			item.setDesconto(porcentagem);
+			break;
+		case REMOVERDESCONTO:
+			item.removerDesconto();
+			break;
+		default:
+			break;
+		}
+	}
+	
+	private Lanche buscarItem(String identificador) {
+		
+		Lanche itemEncontrado = null;
+		
+		for (Lanche item : this.cardapio) {
+			if (item.getIdentificador().equals(identificador) ) {
+				itemEncontrado = item;
+			}
+		}
+		return itemEncontrado;
 	}
 }
