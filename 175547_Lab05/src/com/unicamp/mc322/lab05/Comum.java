83ce884f0581ce5e2402383191b90b28ca74836b
paulocesarvasco <paulocvasco.93@gmail.com>
2021-05-01 15:58:14 -0300

    Class for commons reminders

---
+++ 175547_Lab05/src/com/unicamp/mc322/lab05/Comum.java
@@ -0,0 +1,14 @@
+package com.unicamp.mc322.lab05;
+
+public class Comum {
+
+	private String descricao;
+	
+	public Comum (String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public void imprimir() {
+		System.out.println(this.descricao);
+	}
+}
