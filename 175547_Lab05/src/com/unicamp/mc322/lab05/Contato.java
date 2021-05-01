ab741a49c081dad8cf25e93091ce37b30211d95f
paulocesarvasco <paulocvasco.93@gmail.com>
2021-05-01 15:59:05 -0300

    Class for contacts

---
+++ 175547_Lab05/src/com/unicamp/mc322/lab05/Contato.java
@@ -0,0 +1,14 @@
+package com.unicamp.mc322.lab05;
+
+public class Contato {
+
+	private String nome;
+	private String email;
+	private String telefone;
+
+	public Contato(String nome, String email, String telefone) {
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+	}
+}
