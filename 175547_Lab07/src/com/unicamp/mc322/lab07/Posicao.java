cb0dc9a1f5bcc90e62b149ef62709590662d8337
paulocesarvasco <paulocvasco.93@gmail.com>
2021-05-15 10:57:43 -0300

    Create class 'Posicao'

---
+++ 175547_Lab07/src/com/unicamp/mc322/lab07/Posicao.java
@@ -0,0 +1,11 @@
+package com.unicamp.mc322.lab07;
+
+public class Posicao {
+
+	private char position[] = new char[2];
+	
+	public void alterarPosicao(char a, char b) {
+		this.position[0] = a;
+		this.position[1] = b;
+	}
+}
