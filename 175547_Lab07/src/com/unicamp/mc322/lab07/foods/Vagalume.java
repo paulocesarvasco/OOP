8104c625ecc38ba0b8fdae0e33f58d030a4a42ad
paulocesarvasco <paulocvasco.93@gmail.com>
2021-05-16 10:50:34 -0300

    Implement food class

---
+++ 175547_Lab07/src/com/unicamp/mc322/lab07/foods/Vagalume.java
@@ -0,0 +1,12 @@
+package com.unicamp.mc322.lab07.foods;
+
+public class Vagalume extends Food {
+
+	public Vagalume(int x, int y) {
+		super();
+		super.nome = "Vagalume";
+		super.satisfacao = 15;
+		super.posicao[0] = x;
+		super.posicao[1] = y;
+	}
+}
