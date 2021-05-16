901c7866f9438898e69ad78858ec5eb6637a908d
paulocesarvasco <paulocvasco.93@gmail.com>
2021-05-16 17:11:52 -0300

    Add methods to base class frogs

--- 175547_Lab07/src/com/unicamp/mc322/lab07/frogs/Frog.java
+++ 175547_Lab07/src/com/unicamp/mc322/lab07/frogs/Frog.java
@@ -3,17 +3,40 @@ package com.unicamp.mc322.lab07.frogs;
 public abstract class Frog {
 
 	protected String nome;
-	protected int satisfacao = 0;
+	protected double satisfacao = 0;
 	protected int[] posicaoAnterior = new int[2];
 	protected int[] posicaoAtual = new int[2];
-	
-	protected abstract void atualizarPosicao(int x, int y);
+
+	protected void atualizarPosicao(int x, int y) {
+		this.posicaoAnterior[0] = this.posicaoAtual[0];
+		this.posicaoAnterior[1] = this.posicaoAtual[1];
+		this.posicaoAtual[0] = x;
+		this.posicaoAtual[1] = y;
+	}
 
 	public abstract boolean moverCima();
-	
+
 	public abstract boolean moverBaixo();
-	
+
 	public abstract boolean moverEsquerda();
-	
+
 	public abstract boolean moverDireita();
+
+	public abstract void aumentarSatisfacao(double satisfacao);
+
+	public int[] getPosicaoAtual() {
+		return this.posicaoAtual;
+	}
+
+	public int[] getPosicaoAnterior() {
+		return this.posicaoAnterior;
+	}
+
+	public double getSatisfacao() {
+		return this.satisfacao;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
 }
