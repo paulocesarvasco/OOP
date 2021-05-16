05bd13c6862acbe1c46709eed1ef6697ee270fc6
paulocesarvasco <paulocvasco.93@gmail.com>
2021-05-16 17:15:02 -0300

    Add static methos to use in maps

---
+++ 175547_Lab07/src/com/unicamp/mc322/lab07/map/Utils.java
@@ -0,0 +1,21 @@
+package com.unicamp.mc322.lab07.map;
+
+public final class Utils {
+
+	private static int mapSize = 10;
+	private static int satisfacaoVagalume = 15;
+	private static int satisfacaoGrilo = 20;
+	
+	public static int getMapSize() {
+		return mapSize;
+	}
+	
+	public static int getSatisfacaoVagalume() {
+		return satisfacaoVagalume;
+	}
+	
+	public static int getSatisfacaoGrilo() {
+		return satisfacaoGrilo;
+	}
+}
+
