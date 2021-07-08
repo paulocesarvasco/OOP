c4f43ba51edbb71f253b6fe37fe3c3584fb64331
paulocesarvasco <paulocvasco.93@gmail.com>
2021-07-08 03:51:21 -0300

    Change visibility variable

--- 175547_Projeto/src/com/unicamp/mc322/projeto/cards/Follower.java
+++ 175547_Projeto/src/com/unicamp/mc322/projeto/cards/Follower.java
@@ -1,17 +1,19 @@
 package com.unicamp.mc322.projeto.cards;
 
+import com.unicamp.mc322.projeto.actions.Actions;
 import com.unicamp.mc322.projeto.constants.Effects;
 import com.unicamp.mc322.projeto.constants.Followers;
 import com.unicamp.mc322.projeto.constants.Traces;
+import com.unicamp.mc322.projeto.constants.TypeCards;
 import com.unicamp.mc322.projeto.models.Attributes;
 import com.unicamp.mc322.projeto.utils.FollowersMap;
 
-public class Follower extends Card {
+public class Follower extends Card implements Actions {
 
-	private Integer attack;
-	private Integer defense;
-	private Effects effect;
-	private Traces trace;
+	protected Integer attack;
+	protected Integer defense;
+	protected Effects effect;
+	protected Traces trace;
 	
 	public Follower() {
 		super();
@@ -19,6 +21,7 @@ public class Follower extends Card {
 		Followers followerName = randomFollower();
 		super.name = followerName.toString();
 		super.cost = getAttributes(followerName).getCost();
+		super.type = TypeCards.CREATURE;
 		attack = getAttributes(followerName).getAttack();
 		defense = getAttributes(followerName).getDefense();
 		effect = getEffect(followerName);
@@ -46,5 +49,19 @@ public class Follower extends Card {
 	
 	public String getName() {
 		return name;
+	}
+	
+	public int attack() {
+		return attack;
+	}
+
+	public boolean defense(int counterAttack) {
+		boolean isAlive = true;
+		
+		life -= counterAttack;
+		if(life <= 0) {
+			isAlive = false;
+		}
+		return isAlive;
 	}
 }
