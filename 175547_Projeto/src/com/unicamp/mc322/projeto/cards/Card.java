30c0f07476ffb2661e70be9a7c03fb9a56fdb347
paulocesarvasco <paulocvasco.93@gmail.com>
2021-07-08 03:57:13 -0300

    Add methods to retrieve infos from superclass

--- 175547_Projeto/src/com/unicamp/mc322/projeto/cards/Card.java
+++ 175547_Projeto/src/com/unicamp/mc322/projeto/cards/Card.java
@@ -1,10 +1,23 @@
 package com.unicamp.mc322.projeto.cards;
 
+import com.unicamp.mc322.projeto.constants.TypeCards;
+
 public abstract class Card {
 	
 	protected String name;
 	protected Integer cost;
+	protected Integer life;
+	protected TypeCards type;
 	
+	public TypeCards getType() {
+		return type;
+	}
 	
+	public int getCost() {
+		return cost;
+	}
 	
+	public String getName() {
+		return name;
+	}
 }
